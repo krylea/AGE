d5e7101822f67d885ba6c487c0a410a2ec935419
Kira Selby <kaselby@uwaterloo.ca>
2023-03-22 12:51:29 -0400

    fid scores

--- tools/get_scores.py
+++ tools/get_scores.py
@@ -166,7 +166,7 @@ def get_class_generations(net, dataset, num_source_images, num_generations):
         codes=sampler(outputs, dist, test_opts)
         with torch.no_grad():
             res0 = net.decode(codes, randomize_noise=False, resize=opts.resize_outputs)
-        return res0
+        return res0.cpu().detach()
     
     source_images = [dataset[i] for i in random.choices(range(len(dataset)), k=num_source_images)]
 
