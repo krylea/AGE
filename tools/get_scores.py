27153df0af019d5dca9d19bb8b78b406b2f1702b
Kira Selby <kaselby@uwaterloo.ca>
2023-03-27 17:18:51 -0400

    eval

--- tools/get_scores.py
+++ tools/get_scores.py
@@ -246,7 +246,7 @@ def evaluate_fid_all(datasets, generator, reference_size, sampler, device=torch.
 def evaluate_scores_all(datasets, generator, reference_size, sampler, device=torch.device("cuda"), num_images=-1, 
          image_size=-1):
     
-    lpips_scores = evaluate_scores_by_class(datasets, generator, reference_size, sampler, metrics=('lpips'), 
+    lpips_scores = evaluate_scores_by_class(datasets, generator, reference_size, sampler, metrics=('lpips',), 
                                             num_images=num_images, image_size=image_size)['lpips']#.mean().item()
     
     fid_scores = evaluate_fid_all(datasets, generator, reference_size, sampler, num_images=num_images, image_size=image_size)
