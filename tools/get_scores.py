7e36c937011476243bccc601b55c81c1882f741e
Kira Selby <kaselby@uwaterloo.ca>
2023-03-27 16:05:33 -0400

    eval

--- tools/get_scores.py
+++ tools/get_scores.py
@@ -298,7 +298,7 @@ if __name__=='__main__':
     test_datasets = ImagesDataset.from_folder_by_category(source_root=dataset_args['test_source_root'], opts=opts, transforms=transform)
     
     time = datetime.datetime.now()
-    outfile = os.path.join(test_opts.output_path, "%d_%d_%d_.txt" % (time.month, time.day, time.hour))
+    outfile = os.path.join(test_opts.output_path, "%d_%d_%d%d_.txt" % (time.month, time.day, time.hour, time.minute))
 
     evaluate_scores = evaluate_scores_by_class if not opts.combine_fid else evaluate_fid_all
 
