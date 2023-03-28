dda17b574bff64fb8c98d6e24f959d64055c6e01
Kira Selby <kaselby@uwaterloo.ca>
2023-03-28 12:43:32 -0400

    eval

--- run_eval.sh
+++ run_eval.sh
@@ -26,8 +26,8 @@ pretrained_model_dir="pretrained_models"
 psp_checkpoint_path="${pretrained_model_dir}/psp_${dataset}.pt"
 age_checkpoint_path="${pretrained_model_dir}/age_${dataset}.pt"
 
-class_embedding_path="class_embeds/${run_name}"
-n_distribution_path="n_distribution/${run_name}"
+class_embedding_path="class_embeds/${run_name}/class_embeddings.pt"
+n_distribution_path="n_distribution/${run_name}/n_distribution.npy"
 
 dataset_path="../setgan2/datasets/animal_faces"
 
@@ -45,7 +45,8 @@ argstring="--name=$name \
 --alpha=1 \
 --beta=0.005 \
 --output_size=$output_size
---kshot=$kshot"
+--kshot=$kshot
+--combine_fid"
 
 if [ $renorm -eq 1 ]
 then
