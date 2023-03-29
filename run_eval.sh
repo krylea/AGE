3a0cea4022c8a4f742d38f9fbb3717915f575bbb
Kira Selby <kaselby@uwaterloo.ca>
2023-03-29 12:32:06 -0400

    eval

--- run_eval.sh
+++ run_eval.sh
@@ -26,8 +26,8 @@ pretrained_model_dir="pretrained_models"
 psp_checkpoint_path="${pretrained_model_dir}/psp_${dataset}.pt"
 age_checkpoint_path="${pretrained_model_dir}/age_${dataset}.pt"
 
-class_embedding_path="class_embeds/${run_name}/class_embeddings.pt"
-n_distribution_path="n_distribution/${run_name}/n_distribution.npy"
+class_embedding_path="class_embeds/${run_name}/class_embeddings_${output_size}.pt"
+n_distribution_path="n_distribution/${run_name}/n_distribution_${output_size}.npy"
 
 dataset_path="../setgan2/datasets/animal_faces"
 
